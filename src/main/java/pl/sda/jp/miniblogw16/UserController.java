1f2d6f6f7fe08d9ea1b2674be106246682023619
Bootcamp Szkolenia <bootcamp>
2019-06-29 13:11:30 +0200

    Saving user.

--- src/main/java/pl/sda/jp/miniblogw16/UserController.java
+++ src/main/java/pl/sda/jp/miniblogw16/UserController.java
@@ -1,14 +1,23 @@
 package pl.sda.jp.miniblogw16;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import pl.sda.jp.miniblogw16.user.RegisterForm;
+import pl.sda.jp.miniblogw16.user.UserEntity;
+import pl.sda.jp.miniblogw16.user.UserRepository;
 
 @Controller
 public class UserController {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/register")
     public String showRegisterForm() {
@@ -21,7 +30,13 @@ public class UserController {
     public String handleRegisterForm(
             @ModelAttribute RegisterForm registerForm
     ) {
-        System.out.println(registerForm);
+        UserEntity user = new UserEntity();
+        user.setFirstName(registerForm.getFirstName());
+        user.setLastName(registerForm.getLastName());
+        user.setEmail(registerForm.getEmail());
+        user.setPassword(registerForm.getPassword());
+
+        userRepository.save(user);
 
         return "registerForm";
     }
